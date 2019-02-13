851f7174279f4ae2348b400dc794530befb6d177
lilin <1142114078@qq.com>
2019-02-13 22:37:14 +0800

    das

---
+++ src/main/java/com/sunyard/ribbonconsumer/RibbonConsumerApplication.java
@@ -0,0 +1,26 @@
+package com.sunyard.ribbonconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class RibbonConsumerApplication {
+
+	//开启服务端负载均衡能力
+	@Bean
+	@LoadBalanced
+	RestTemplate  restTemplate(){
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonConsumerApplication.class, args);
+	}
+
+}
+
